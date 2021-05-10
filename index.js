438bd02c3922622d75c197eec9bcc99bedc60775
sravittxof <travis.fox90@gmail.come>
2021-05-10 02:24:18 -0400

    Declare saySomething and getQuote functions. Change options elements to use value of guest_id.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const allPeople = document.getElementById("all-people");
 const guestList = document.getElementById("Guest List");
+const guestIndexUrl = "http://localhost:3000/guests";
 
 function allHistoricalGuests(){
-    fetch("http://localhost:3000/guests")
+    fetch(guestIndexUrl)
     .then(resp => resp.json())
     .then(json => getHistoricalPeople(json))
 }
@@ -10,20 +11,21 @@ function allHistoricalGuests(){
 function getHistoricalPeople(data){
     data.forEach(element => {
         const personElement = document.createElement("option");
-        personElement.value = element["name"];
+        personElement.value = element["id"];
         personElement.innerText = element["name"];
-        allPeople.appendChild(personElement);
+        allPeople.append(personElement);
 
     });    
 }
 
-
 allHistoricalGuests();
 
-
 function selectGuest(input){
-    console.log(input.target.value);
-    inviteSelectedGuest(input.target.value)
+    const guest_id = input.target.value;
+    const guestName = document.querySelector("option[value=" +  `\"${guest_id}\"]`).innerText;
+    const guest = {id: guest_id, name: guestName }
+
+    inviteSelectedGuest(guest)
 }
 
 allPeople.addEventListener("change", input => selectGuest(input))
@@ -31,6 +33,19 @@ allPeople.addEventListener("change", input => selectGuest(input))
 
 function inviteSelectedGuest(guest){
     guestListItem = document.createElement("li");
-    guestListItem.innerText = guest;
+    guestListItem.value = guest["id"];
+    guestListItem.innerText = guest["name"];
     guestList.appendChild(guestListItem);
+    guestListItem.addEventListener("click", speaker => saySomething(speaker))
+}
+
+function saySomething(speaker){
+    getQuote(speaker);
+}
+
+function getQuote(speaker){
+    guestId = speaker.target.value;
+    fetch("http://localhost:3000/quotes/" + `${guestId}`)
+    .then(resp => resp.json())
+    .then(json => console.log(json)) 
 }
