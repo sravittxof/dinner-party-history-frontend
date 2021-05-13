ef11032679f67598fa7cc4ecae87e811d6f8f66c
sravittxof <travis.fox90@gmail.come>
2021-05-12 21:55:24 -0400

    Created src directory for class files. Moved created Party class and move functions to file.

--- src/party.js
+++ src/party.js
@@ -1,7 +1,39 @@
-class party{
+class Party{
+
+    constructor(id){
+        this.id = id;
+    }
+
+    static render(json){
+        const partyObj = new Party(json["id"]);
+        return partyID.innerText = "Party Number: " + partyObj.id;
+    }
+
+    static newParty(){
+        PartyAPI.createParty();
+    }
+
+}
 
 
 
-
+class PartyAPI {
     
+    static createParty(){
+        const objURL = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: ""
+        };
+        fetch(url + "/parties", objURL)
+        .then(response => response.json())
+        .then(function(json) {
+            console.log(json)
+            Party.render(json);
+        })
+    }
+
 }
